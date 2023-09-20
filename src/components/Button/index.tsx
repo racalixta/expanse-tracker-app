f12faf017e4d891156f80425a16b7d972960a375
racalixta <ra.calixta@gmail.com>
2023-09-19 21:29:39 -0300

    feat: dashboard page and ghost button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,33 @@
-import { ButtonHTMLAttributes, ReactNode } from "react"
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import tw from "tailwind-styled-components";
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement>  {
   children: ReactNode;
+  variant?: 'ghost';
 }
 
-export const Button = ({ children, ...props }: Props) => {
-  return (
-    <button 
-      {...props} 
-      className='bg-indigo-700 h-10 border rounded-md w-full text-white font-semibold text-lg hover:bg-indigo-500 disabled:bg-gray-400 disabled:cursor-not-allowed'
-    >
+export const Button = ({ children, variant, ...props }: Props) => {
+  if(variant === 'ghost') return (
+    <GhostButton {...props}>
       {children}
-    </button>
+    </GhostButton>
+  )
+
+  return (
+    <MainButton {...props}>
+      {children}
+    </MainButton>
   )
 }
+
+export const BaseButton = tw.button`
+  h-10 rounded-md w-full text-white font-semibold px-8 text-lg  disabled:bg-gray-400 disabled:cursor-not-allowed
+`;
+
+export const MainButton = tw(BaseButton)`
+  bg-indigo-700 hover:bg-indigo-500
+`;
+
+export const GhostButton = tw(BaseButton)`
+  bg-transparent border-2 border-indigo-700 text-indigo-700 hover:bg-indigo-200
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { TemplateContainer } from '../../components/templates'
+import { Button } from '../../components/Button'
 
 export const Dashboard  = () => {
   return (
-    <div>
-      dashboard 
-    </div>
+    <TemplateContainer>
+      <header className='w-full max-w-screen-lg flex justify-between h-1/6 imtes-center py-4 '>
+        <span className='font-bold text-indigo-700 text-2xl'>Expense Tracker</span>
+
+        <nav className='flex w-7/12 gap-4'>
+          <Button>Adicionar Conta</Button>
+          <Button variant='ghost'>Sair</Button>
+        </nav>
+      </header>
+    </TemplateContainer>
   )
 }
 
